274ffff118229b8e115f1a4d69319d3cc445e73f
hc959678 <34315540+hc959678@users.noreply.github.com>
2018-10-20 17:51:35 -0500

    Add files via upload

---
+++ Models/Registration.cs
@@ -0,0 +1,43 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PRPC_CIDM4390.Models
+{
+
+    public class Registration
+    {
+
+        [Required()]
+        public string username { get; set;}
+
+        [DataType(DataType.Password)]
+        [StringLength(255, MinimumLength = 8)]
+        [Required()]
+        
+        public string Password { get; set;}
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [StringLength(255, MinimumLength = 8)]
+        [Required()]
+    
+        public string ConfirmPassword {get; set;}
+
+
+        public string firstname {get; set;}
+
+        public string lastname {get; set;}
+
+        public string birthday {get; set;}
+
+        [DataType(DataType.EmailAddress)]
+        [StringLength(128)]
+        [Required()]
+        public string email {get; set;}
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression("^[0-9]{8}$")]
+        [StringLength(32)]
+        public string PhoneNumber {get; set;}
+
+        
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -56,7 +56,7 @@ namespace PRPC_CIDM4390
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Registration}/{action=Index}/{id?}");
             });
         }
     }
