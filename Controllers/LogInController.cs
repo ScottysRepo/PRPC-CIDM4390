abd827cf1ff7628b62153d8771286954aa6abd59
hc959678 <34315540+hc959678@users.noreply.github.com>
2018-10-29 13:53:53 -0500

    Add files via upload

---
+++ Controllers/LogInController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PRPC_CIDM4390.Models;
+
+
+namespace PRPC_CIDM4390.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            // waiting for Registration info and database to add
+            //var userName = new {UserName = model.userName};
+           // string userName = "user";
+           // if(userName= "user"){
+              //  var loggingIn = true;
+           // }
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Message"] = "Register Here";
+
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+       
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/LogIn.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PRPC_CIDM4390.Models
+{
+    public class LogIn 
+    {
+        public string ID {get; set;}
+        [Required]
+        [Display(Name="User Name")]
+        public string userName { get; set; }
+        public string password {get; set;}
+        
+    }
+}
